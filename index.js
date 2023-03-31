59f3cc21dcb96f81e1b94bcb3f3098330e2bede0
manu00-droid <manavsidana00@gmail.com>
2023-04-01 01:54:37 +0530

    fix: grid colors on dark mode

--- index.js
+++ index.js
@@ -1,3 +1,155 @@
+// Money Chart =========================================================//
+
+
+
+
+
+const moneyCanvas = document.getElementById('money-chart');
+
+const moneyData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [{
+        label: 'Money',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+};
+
+
+
+const DISPLAY = true;
+const BORDER = true;
+const CHART_AREA = true;
+const TICKS = true;
+
+const moneyConfig = {
+    type: 'line',
+    data: moneyData,
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                border: {
+                    display: true,
+                },
+
+                grid: {
+                    display: DISPLAY,
+                    drawOnChartArea: CHART_AREA,
+                    drawTicks: TICKS,
+                    color: "#707070"
+
+                }
+            },
+            y: {
+                border: {
+                    display: true,
+                },
+                grid: {
+                    color: "#707070"
+                }
+            }
+        }
+    },
+};
+const moneyChart = new Chart(moneyCanvas, moneyConfig);
+
+
+
+
+// Money Chart =========================================================//
+
+
+//electricity chart =========================================================//
+
+const electricityCanvas = document.getElementById('electricity-chart');
+
+
+const electricityData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [{
+        label: 'Electricity Generated',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+};
+
+
+const electricityConfig = {
+    type: 'line',
+    data: electricityData,
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                border: {
+                    display: BORDER
+                },
+
+                grid: {
+                    display: DISPLAY,
+                    drawOnChartArea: CHART_AREA,
+                    drawTicks: TICKS,
+                    color: "#707070"
+                }
+            },
+            y: {
+                border: {
+                    display: true
+                },
+                grid: {
+                    color: "#707070"
+                }
+            }
+        }
+    },
+};
+
+const electricityChart = new Chart(electricityCanvas, electricityConfig);
+
+
+
+
+//electricity chart =========================================================//
+
+
+
+
+
+
+
+// updating the data of charts =========================================================//
+
+
+
+setInterval(() => {
+    const newData = [0, 10, 20, 30, 40];
+    moneyChart.data.datasets[0].data = newData;
+    moneyChart.update();
+}, 1000); // update every 1 seconds
+
+
+
+// updating the data of charts =========================================================//
+
+
+
+
+
+
+
+
+
+
+//Toggling dark mode and sidebar =========================================================//
+
+
+
+
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
     toggle = body.querySelector(".toggle"),
@@ -11,7 +163,12 @@ toggle.addEventListener("click", () => {
 })
 
 searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
+    if (sidebar.classList.contains("close")) {
+        sidebar.classList.remove("close");
+    }
+    else {
+        sidebar.classList.add("close");
+    }
 })
 
 modeSwitch.addEventListener("click", () => {
@@ -19,169 +176,19 @@ modeSwitch.addEventListener("click", () => {
 
     if (body.classList.contains("dark")) {
         modeText.innerText = "Light mode";
+        // moneyChart.options.scales.x.grid.color = "#707070";
+        // moneyChart.options.scales.y.grid.color = "#707070";
+        // electricityChart.options.scales.x.grid.color = "#707070";
+        // electricityChart.options.scales.y.grid.color = "#707070";
     } else {
         modeText.innerText = "Dark mode";
+        // moneyChart.options.scales.x.grid.color = "#707070";
+        // moneyChart.options.scales.y.grid.color = "#707070";
+        // electricityChart.options.scales.x.grid.color = "#707070";
+        // electricityChart.options.scales.y.grid.color = "#707070";
 
     }
 });
 
 
-
-
-
-
-// // // Get the canvas elements for the charts
-// const moneyCanvas = document.getElementById('money-chart');
-// const electricityCanvas = document.getElementById('electricity-chart');
-
-// // Create the line charts using Chart.js
-// const moneyChart = new Chart(moneyCanvas, {
-//     type: 'line',
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'Money Saved',
-//             data: [100, 150, 200, 250, 300, 350, 400],
-//             borderColor: '#3e95cd',
-//             fill: false
-//         }]
-//     },
-//     options: {
-//         responsive: true,
-//         title: {
-//             display: false
-//         },
-//         legend: {
-//             display: false
-//         },
-//         scales: {
-//             xAxis: [{
-//                 scaleLabel: {
-//                     display: true,
-//                     labelString: 'Month'
-//                 }
-//             }],
-//             yAxis: [{
-//                 scaleLabel: {
-//                     display: true,
-//                     labelString: 'Money Saved'
-//                 }
-//             }]
-//         }
-//     }
-// });
-
-// const electricityChart = new Chart(electricityCanvas, {
-//     type: 'line',
-//     data: {
-//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//         datasets: [{
-//             label: 'Electricity Generated',
-//             data: [200, 250, 300, 350, 400, 450, 500],
-//             borderColor: '#8e5ea2',
-//             fill: false
-//         }]
-//     },
-//     options: {
-//         responsive: true,
-//         title: {
-//             display: false
-//         },
-//         legend: {
-//             display: false
-//         },
-//         scales: {
-//             xAxes: [{
-//                 scaleLabel: {
-//                     display: true,
-//                     labelString: 'Month'
-//                 }
-//             }],
-//             yAxes: [{
-//                 scaleLabel: {
-//                     display: true,
-//                     labelString: 'Electricity Generated'
-//                 }
-//             }]
-//         }
-//     }
-// });
-
-
-// Get the canvas elements for the charts
-// const moneyChart = document.getElementById('money-chart');
-// const electricityChart = document.getElementById('electricity-chart');
-// change colors of the charts when in dark mode
-// if (body.classList.contains("dark")) {
-//     moneyChart.data.datasets[0].borderColor = "#fff";
-//     electricityChart.data.datasets[0].borderColor = "#fff";
-//     moneyChart.update();
-//     electricityChart.update();
-// }
-
-
-
-
-
-const moneyCanvas = document.getElementById('money-chart');
-const electricityCanvas = document.getElementById('electricity-chart');
-
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    datasets: [{
-        label: 'Money',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-    }]
-};
-// const config = {
-//     type: 'line',
-//     data: data,
-// };
-
-// Change these settings to change the display for different parts of the X axis
-// grid configuration
-const DISPLAY = true;
-const BORDER = true;
-const CHART_AREA = true;
-const TICKS = true;
-
-const config = {
-    type: 'line',
-    data: data,
-    options: {
-        responsive: true,
-        scales: {
-            x: {
-                border: {
-                    display: BORDER
-                },
-
-                grid: {
-                    display: DISPLAY,
-                    drawOnChartArea: CHART_AREA,
-                    drawTicks: TICKS,
-                }
-            },
-            y: {
-                border: {
-                    display: true
-                },
-
-                grid: {
-                },
-            }
-        }
-    },
-};
-const moneyChart = new Chart(moneyCanvas, config);
-
-
-
-setInterval(() => {
-    const newData = [0, 10, 20, 30, 40];
-    moneyChart.data.datasets[0].data = newData;
-    moneyChart.update();
-}, 1000); // update every 5 seconds
+//Toggling dark mode and sidebar =========================================================//
